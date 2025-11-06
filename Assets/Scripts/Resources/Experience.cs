bf6536a581f5e37ad7edf40200761d49096aef17
Ahmadullah Naibi <ahmadullah@hotmail.dk>
2025-11-06 14:15:10 +0100

    fixed experience

--- Assets/Scripts/Resources/Experience.cs
+++ Assets/Scripts/Resources/Experience.cs
@@ -6,17 +6,17 @@ namespace RPG.Resources
     {
         [SerializeField] float experiencePoints = 0;
 
+        public float ExperiencePoints => experiencePoints;
+
         public void GainExperience(float experience)
         {
             if (experience < 0)
-+           {
-+               Debug.LogWarning("Cannot gain negative experience");
-+               return;
-+           }
+            {
+                Debug.LogWarning("Cannot gain negative experience");
+                return;
+            }
             experiencePoints += experience;
         }
-
-        public float ExperiencePoints => experiencePoints;
 
     }
 }
